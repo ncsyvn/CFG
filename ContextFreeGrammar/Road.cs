aa195f5610e1fe94714a8606d8caa94f499434e2
ncsyvn98 <sy100198@gmail.com>
2019-05-18 17:46:21 +0700

    Initialize ripository

---
+++ ContextFreeGrammar/Road.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextFreeGrammar
+{
+    public class Road
+    {
+        private string start;
+        private string end;
+
+        public string Start { get => start; set => start = value; }
+        public string End { get => end; set => end = value; }
+    }
+}
---
+++ ContextFreeGrammar/StartFrom.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ContextFreeGrammar
+{
+    partial class StartFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStandardized = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(104, 33);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(131, 120);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input CFG";
+            // 
+            // textBoxStandardized
+            // 
+            this.textBoxStandardized.Location = new System.Drawing.Point(104, 222);
+            this.textBoxStandardized.Multiline = true;
+            this.textBoxStandardized.Name = "textBoxStandardized";
+            this.textBoxStandardized.ReadOnly = true;
+            this.textBoxStandardized.Size = new System.Drawing.Size(136, 120);
+            this.textBoxStandardized.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Standardized";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(297, 222);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(239, 20);
+            this.textBoxFilePath.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Path";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(542, 220);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(62, 23);
+            this.buttonOpenFile.TabIndex = 7;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Location = new System.Drawing.Point(134, 357);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(75, 23);
+            this.buttonMin.TabIndex = 8;
+            this.buttonMin.Text = "Min";
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMin_Click);
+            // 
+            // StartFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonMin);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxStandardized);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "StartFrom";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStandardized;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonMin;
+    }
+}
+
